c63afc8bfa41bf501d349a9e3b744b929259ce06
mq <mq@162.com>
2016-06-18 01:59:58 +0800

    .m

--- WeiBo/WeiBo/Classes/Other-其他/main.m
+++ WeiBo/WeiBo/Classes/Other-其他/main.m
@@ -1,9 +1,9 @@
 //
 //  main.m
-//  WeiBo
+//  BuDeJie15
 //
-//  Created by 甘明强 on 16/6/18.
-//  Copyright © 2016年 小码哥. All rights reserved.
+//  Created by xiaomage on 16/6/14.
+//  Copyright © 2016年 seemygo. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -14,3 +14,19 @@ int main(int argc, char * argv[]) {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+
+// main - >UIApplicationMain
+
+/*
+    UIApplicationMain底层实现
+    1.创建UIApplication对象(1.打开网页 2.设置联网状态 3.设置状态栏 4.设置提醒数字)
+    2.创建UIApplication代理对象(1.监听应用程序生命周期 2.内存警告)
+    3.开启主运行循环(保证程序一直运行),监听事件,runloop(1.每个线程都有runloop,但是主线程的runloop自动开启)
+    4.判断info.plist有没有指定main,有,就会去加载main.storyboard
+ 
+ 
+    加载main.storyboard
+    1.创建窗口
+    2.设置窗口根控制器
+    3.显示窗口
+ */
