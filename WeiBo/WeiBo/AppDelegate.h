b07d8b0bf08fb6fe13d1b255921dd69b299902fe
mq <mq@162.com>
2016-06-18 00:41:17 +0800

    初始化项目

---
+++ WeiBo/WeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiBo
+//
+//  Created by 甘明强 on 16/6/18.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
